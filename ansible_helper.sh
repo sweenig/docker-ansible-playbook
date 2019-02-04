2b05f63b84ff77ad297a924a216324aa0d8abe1f
Stuart Weenig <stuart@weenig.com>
2019-02-04 14:59:07 -0600

    Updated to make it mine

--- ansible_helper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker run --rm -it \
-  -v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
-  -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub \
-  -v $(pwd):/ansible/playbooks \
-  -v /var/log/ansible/ansible.log \
-  walokra/ansible-playbook "$@"
